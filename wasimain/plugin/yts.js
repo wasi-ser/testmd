7cf86f66913269728eda8d26008c7ede8ae706f1
wasi-ser <itxmewasi@gmail.com>
2024-08-09 14:24:24 +0100

    Update yts.js

--- wasimain/plugin/yts.js
+++ wasimain/plugin/yts.js
@@ -1,5 +1,5 @@
 import yts from 'yt-search';
-import ytdl from 'ytdl-core';
+import ytdl from 'wasitech';
 import pkg, { prepareWAMessageMedia } from '@whiskeysockets/baileys';
 const { generateWAMessageFromContent, proto } = pkg;
 
